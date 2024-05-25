94dd2ae95860fe78ff34a7d309e681ceea415330
Moises Almeida <moisesssprg@gmail.com>
2024-05-25 00:24:51 -0300

    adiciona css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
 
 export function App() {
 
   return (
-    <Post 
-      author="Paulo Nobre" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim maiores corrupti facere error, placeat eius rem aliquid tenetur quam aperiam quod ipsam cumque consequuntur saepe sapiente expedita, sequi libero? Tempore?" 
-    />
+    <div>
+      <Header />
+  
+      <Post 
+        author="Paulo Nobre" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim maiores corrupti facere error, placeat eius rem aliquid tenetur quam aperiam quod ipsam cumque consequuntur saepe sapiente expedita, sequi libero? Tempore?" 
+      />
+    </div>
   )
 }
